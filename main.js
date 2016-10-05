b58452e5d4b1bd3e850db63ef79ac8a3d15cc0f4
Jack Storrs <jackstorrs8@gmail.com>
2016-10-05 10:56:19 -0400

    Calculator with button functionality before testing out removing all the event parameters from my functions.

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-var answerBox = document.getElementById("answer");
+ var answerBox = document.getElementById("answer");
 
 var calculate = function (event) {
   event.preventDefault ();
-  answerBox.innerHTML = ""
+  // answerBox.innerHTML = ""
   var val1 = Number(document.getElementById("value-one").value);
   var val2 = Number(document.getElementById("value-two").value);
   answerBox.innerHTML += val1 + val2;
 };
 
 var calcButton = document.getElementById("calculate-button");
-
 calcButton.addEventListener("click", calculate);
